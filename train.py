cd5854a38d4bd31525345b89dfed176422ecd5fa
AILabs <mega4alik@gmail.com>
2022-08-08 16:12:26 +0600

    Update train.py

--- train.py
+++ train.py
@@ -4,12 +4,16 @@ from data import PLDataModule
 from model import PLAggregate
 from train_configs import CFG
 
+import os
+
+os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"   # see issue #152
+os.environ["CUDA_VISIBLE_DEVICES"]="0"
 
 if __name__=="__main__":
   pl_datamodule = PLDataModule(CFG)
-  pl_model = PLAggregate()
+  pl_model = PLAggregate(CFG)
 
-  pl_model.model.video2feature.s3d_load_weights(CFG.S3D_weight_path)
+  pl_model.model.video2feature.s3d_load_weights(CFG.S3D_weights)
   
   trainer = Trainer(**CFG.trainer_configs)
   trainer.fit(model=pl_model, datamodule=pl_datamodule)
